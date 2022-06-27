d0161c9e4b53fe958088669c369dd63d24873d5e
rvanemmerik <20114982@student.hhs.nl>
2022-06-27 19:33:02 +0200

    Opdrachten

--- 04Animation/scripts/main.js
+++ 04Animation/scripts/main.js
@@ -1,12 +1,19 @@
 
 
 // // changing individual properties with code and using setInterval
-// var rotationSpeed = 0.01;
-// var myOtherBox = document.getElementById('myOtherBox');
+var rotationSpeed = 0.005;
+var myOtherBox = document.getElementById('myOtherBox');
 
-// function spin(){
-// 	myOtherBox.object3D.rotation.x += rotationSpeed;
-// 	console.log(myOtherBox.object3D.rotation.x);
-// }
+function spin(){
+    myOtherBox.object3D.rotation.x -= rotationSpeed;
+    myOtherBox.object3D.rotation.y += rotationSpeed;
+    myOtherBox.object3D.rotation.z += rotationSpeed;
+    console.log(myOtherBox.object3D.rotation.x);
+}
 
-// setInterval(spin, 16); //equivalent to 60 fps
+function bounce() {
+    myOtherBox.object3D.position.y += rotationSpeed;
+}
+
+setInterval(spin, 16); //equivalent to 60 fps
+setInterval(bounce, 16);
